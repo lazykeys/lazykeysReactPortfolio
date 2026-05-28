2302adf2362cf0f9d5eac68b32a2e09dc420327f
lazykeys <155578589+lazykeys@users.noreply.github.com>
2026-05-27 23:55:44 -0400

    In-Progress: Left Side Spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="flex flex-col m-8 min-h-full w-auto">
+    <div className="flex flex-col m-8 h-full w-auto">
       <Header></Header>
       <Subheader></Subheader>
       <About></About>
@@ -30,11 +30,11 @@ function Header() {
 
 function Subheader() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col justify-around h-24">
       <h2 className="text-lk-6 font-DigitalDiscoThin text-3xl">Game & Frontend Developer</h2>
 
-      <div className="flex gap-1">
-        <svg className="w-8" viewBox="-3 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-223.000000, -5399.000000)" className="fill-lk-3"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M174,5248.219 C172.895,5248.219 172,5247.324 172,5246.219 C172,5245.114 172.895,5244.219 174,5244.219 C175.105,5244.219 176,5245.114 176,5246.219 C176,5247.324 175.105,5248.219 174,5248.219 M174,5239 C170.134,5239 167,5242.134 167,5246 C167,5249.866 174,5259 174,5259 C174,5259 181,5249.866 181,5246 C181,5242.134 177.866,5239 174,5239" id="pin_fill_sharp_circle-[#000000]"> </path> </g> </g> </g> </g></svg>
+      <div className="flex gap-3">
+        <svg className="w-6 h-8" viewBox="-3 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-223.000000, -5399.000000)" className="fill-lk-3"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M174,5248.219 C172.895,5248.219 172,5247.324 172,5246.219 C172,5245.114 172.895,5244.219 174,5244.219 C175.105,5244.219 176,5245.114 176,5246.219 C176,5247.324 175.105,5248.219 174,5248.219 M174,5239 C170.134,5239 167,5242.134 167,5246 C167,5249.866 174,5259 174,5259 C174,5259 181,5249.866 181,5246 C181,5242.134 177.866,5239 174,5239" id="pin_fill_sharp_circle-[#000000]"> </path> </g> </g> </g> </g></svg>
         <h3 className="text-lk-6 font-DigitalDiscoThin text-3xl">Ohio, USA</h3>
       </div>
     </div>
